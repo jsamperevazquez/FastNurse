d91aaade0c5ea429c2cb13ccded3845a49b0b9bb
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-11-17 23:30:45 +0100

    Creado fragment con maps app

--- app/src/main/java/com/cursoandroid/fastnurse/MainActivity.kt
+++ app/src/main/java/com/cursoandroid/fastnurse/MainActivity.kt
@@ -2,10 +2,23 @@ package com.cursoandroid.fastnurse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 }
